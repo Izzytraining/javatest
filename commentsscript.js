5f8b61fc1a6a892f892d0d9c3085550393d6ca08
Izzytraining <izzyjhalliday23@gmail.com>
2022-12-12 16:23:45 +0000

    changes

---
+++ commentsscript.js
@@ -0,0 +1,46 @@
+//a stamp needs a "pattern" to stamp everytime
+// a function takes "parameters to work"
+
+//return for the answer outside of function
+
+//function: multiplyByTwo
+//parameter: number
+
+//function multiplyByTwo(number){
+//   return number * 2;
+//}
+
+//number toworkon: is an ARGUMENT which will be paased to the function
+//let numberToWorkOn = 10
+//let result = multiplyByTwo();
+
+//return result;
+
+// if a function
+// "\n"
+
+//function multiplayer(num1, num2, num3){
+//  return num1 * num2 * num3;
+
+//}
+//console.log(multiplayer(2, 3, 4));
+
+//function
+
+//let result = ();
+//let topresult = ();
+//theprinter(topresult);
+
+//const multiplicationResult = multiplayer(2,3,4);
+//console.log(multiplicationResult);
+
+//function doesnotacceptparameters(){
+//   console.log("I am a function that is independent.");
+//}
+
+//doesnotacceptparameters();
+
+//self invoking anoymous function
+//  (function(){
+//    console.log("nobodycallsme");
+//  })();
---
+++ script.js
@@ -0,0 +1,58 @@
+//task1
+
+function hello() {}
+console.log("Hello, World");
+
+//task2
+
+function quadrupler(num) {
+  return num * 4;
+}
+console.log(quadrupler(-20));
+
+//task3
+
+function welcome(firstname, lastname) {
+  return "hello $ (firstname) $(lastname), how can I help you?";
+}
+console.log(welcome());
+
+//task4
+
+function tempconvertor(fahrenheit) {
+  let result = ((fahrenheit - 32) * 5) / 9;
+  return result;
+}
+
+console.log(Math.round(tempconvertor(80)));
+
+//task5
+
+//create age and name varibles
+//function dog years, age and name param - 1 year of dog - age = - 15 - 2 year = 9 human years
+//if age 1 = 15
+//else if age 2 = 9
+//else if age  2> = 5
+//else
+//console log - name & age
+
+function myDogage(age, name, dogAge) {
+  dogAge = age * 7;
+  return "My name is ${name}. I am ${myDogage} years old in dog years.";
+}
+console.log(myDogage());
+
+//task6 calcultor
+
+function calculator(argument1, argument2, argument3) {}
+
+//task 7 less than
+
+function min(a, b) {
+  return;
+}
+
+//task 8 the power of
+function pow(x, n) {
+  return;
+}
